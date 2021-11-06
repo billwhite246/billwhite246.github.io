179cb3905c57c0815c12aab34019b55b95e56f61
bill <scompany@vip.qq.com>
2021-11-06 22:07:50 +0800

    Site updated: 2021-11-06 22:07:49

--- js/utils.js
+++ js/utils.js
@@ -218,3 +218,15 @@ function autoImgSize() {
   }
 }
 autoImgSize();
+
+// 直接在recent-posts.pug内
+// 加载Google AdSense
+var googleAds = `
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6606077988466334" crossorigin="anonymous"></script>
+<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6606077988466334" data-ad-slot="3105555854" data-ad-format="auto" data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+`;
+// document.getElementById("my-google-ad").style = "height: 130px;";
+document.getElementById("my-google-ad").innerHTML = googleAds;
